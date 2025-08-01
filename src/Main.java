03ce94b42553dfb7ffedf5021a00908ad246c23e
Azzie Umbay <aumbay15550@liceo.edu.ph>
2025-08-01 11:18:39 +0800

    Number Inputs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
+
+
 public class Main {
-     public static void main(String[] args){
-         System.out.println("Main Branch!");
-     }
+
+    static int number = 0;
+
+    public static void main(String[] args) {
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.print("Enter number " + i+ ": " );
+
+            number = number+scanner.nextInt();
+
+
+
+        }
+
+        System.out.println("Total Number: "+number);
+
+    }
+
 }
