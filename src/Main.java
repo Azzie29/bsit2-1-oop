6b47c710c69647aa235d762d47c439190b48d617
Azzie Umbay <aumbay15550@liceo.edu.ph>
2025-08-04 10:28:27 +0800

    Book

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,117 @@
 public class Main {
-     public static void main(String[] args){
-         System.out.println("Main Branch!");
+
+
+
+     static class Book {
+
+          String title;
+
+          String author;
+
+          int pages;
+
+          boolean isAvailable;
+
+
+
+          public Book(String title, String author, int pages) {
+
+               this.title = title;
+
+               this.author = author;
+
+               this.pages = pages;
+
+               this.isAvailable = true;
+
+               System.out.println("A new book '" + title + "' by " + author + " has been added to the library!");
+
+          }
+
+
+
+          public void displayInfo() {
+
+               System.out.println("\n~~ Book Information ~~");
+
+               System.out.println("Title  : " + title);
+
+               System.out.println("Author : " + author);
+
+               System.out.println("Pages  : " + pages);
+
+               System.out.println("Availability: " + (isAvailable ? "Available" : "Not Available"));
+
+          }
+
+
+
+          public void borrowBook() {
+
+               if (isAvailable) {
+
+                    isAvailable = false;
+
+                    System.out.println("You have successfully borrowed '" + title + "'.");
+
+               } else {
+
+                    System.out.println("'" + title + "' is currently not available.");
+
+               }
+
+          }
+
+
+
+          public void returnBook() {
+
+               if (!isAvailable) {
+
+                    isAvailable = true;
+
+                    System.out.println("Thank you for returning '" + title + "'.");
+
+               } else {
+
+                    System.out.println("'" + title + "' was not borrowed.");
+
+               }
+
+          }
+
      }
+
+
+
+     public static void main(String[] args) {
+
+          Book book1 = new Book("A Court of Thorn and Roses", "Sarah J. Maas", 325);
+
+          Book book2 = new Book("The Housemaid", "Freida McFadden", 480);
+
+          Book book3 = new Book("The Anxious Generation", "Jonathan Haidt", 667);
+
+
+
+          book1.displayInfo();
+
+          book2.displayInfo();
+
+          book3.displayInfo();
+
+
+
+          book1.borrowBook();
+
+          book1.displayInfo();
+
+          book1.borrowBook();
+
+          book1.returnBook();
+
+          book1.displayInfo();
+
+     }
+
 }
