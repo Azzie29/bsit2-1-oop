c38b914284ebc84cb41cede264965030b0600def
Azzie Umbay <aumbay15550@liceo.edu.ph>
2025-08-18 10:03:00 +0800

    lab-activity-4

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+    GradeCalculator calculator = new GradeCalculator();
+
+    double[] grades = {85.5, 92.0, 78.5, 90.0};
+    double average = calculator.calculateAverage(grades);
+    String letterGrade = calculator.getLetterGrade(average);
+
+    calculator.displayResult("John Smith", average);
+    calculator.displayResult("John Smith", average, letterGrade);
+}
